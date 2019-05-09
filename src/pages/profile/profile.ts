e6007e9467cf5e5f46669283f68682ea2e6d67c8
lucascandido-ti <lucascandido.ti@gmail.com>
2019-05-09 15:14:33 -0300

    Fazendo upload da foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -26,6 +26,10 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData(){
     let localUser = this.storage.getLocalUser();
     if(localUser && localUser.email){
       this.clienteService.findByEmail(localUser.email)
@@ -42,6 +46,7 @@ export class ProfilePage {
       this.navCtrl.setRoot('HomePage');
     }
   }
+
 
   getImageIfExists(){
     this.clienteService.getImageFromBucket(this.cliente.id)
@@ -68,7 +73,19 @@ export class ProfilePage {
     }, (err) => {
 
     });
-    
+  }
+
+  sendPicture(){
+    this.clienteService.uploadPicture(this.picture)
+      .subscribe(response => {
+        this.picture = null;
+        this.loadData();
+      },
+      error => {});
+  }
+
+  cancel(){
+    this.picture = null;
   }
 
 }
